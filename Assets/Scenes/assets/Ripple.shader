e19ef49c5397e2a37332ca93cbff27ea2796d33f
Nicholas H <syncopika@users.noreply.github.com>
2022-10-16 16:00:02 -0400

    new visualizer (ripples) (#5)
    * trying a new visualizer with gradients and spheres (kinda working but still more to go)! also trying shaders in Unity :D
    
    * a bit more progress with new visualizer (renamed to ripples now)
    
    * bit more progress
    
    * more progress!
    
    * forgot new demo gif

---
+++ Assets/Scenes/assets/Ripple.shader
@@ -0,0 +1,113 @@
+Shader "Unlit/Ripple"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+
+        _Density("Density", float) = 100
+        _Strength("Strength", float) = 2
+        _Brightness("Brightness", float) = 0.5
+    }
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        //Tags { "RenderType" = "Opaque" }
+        LOD 100
+
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            float2 center;// = float2(0.5, 0.5);
+            float4 color;// = float4(0.3, 0.6, 1, 0.8);
+            float _Speed;
+            float _Density;
+            float _Strength;
+            float _Brightness;
+
+            // freq bin value
+            float freqBinDelta;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+
+                // https://github.com/twostraws/ShaderKit/blob/main/Shaders/SHKCircleWaveBlended.fsh
+                // maybe helpful? https://stackoverflow.com/questions/41405498/how-can-i-make-gradient-sphere-on-glsl
+                float waveSpeed = -(_Time.y * _Speed * 10.0);
+                //if (freqBinDelta != 0) waveSpeed *= freqBinDelta * 5.0;
+
+                float3 brightness = float3(_Brightness, _Brightness, _Brightness);
+                float pixelDistance = distance(i.uv, center);
+
+                if (pixelDistance > 0.3) {
+                    // make transparent
+                    return float4(1.0, 1.0, 1.0, 0);
+                }
+
+                if (color.r == 1 && color.g == 1 && color.b == 1 && color.a == 0) {
+                    return float4(1.0, 1.0, 1.0, 0);
+                }
+
+                float3 gradientColor = float3(color.r, color.g, color.b) * brightness;
+
+                // set gradient color to be a function of the freq bin delta
+                if (freqBinDelta > 0) gradientColor *= freqBinDelta * 2.0;
+
+                float colorStrength = pow(1.0 - pixelDistance, 3.0);
+                colorStrength *= _Strength;
+
+                float waveDensity = _Density * pixelDistance;
+                float cosine = cos(waveSpeed + waveDensity);
+                float cosAdjust = (0.5 * cosine) + 0.5;
+                
+                float lumi = colorStrength * (_Strength + cosAdjust);
+                lumi *= 1.0 - (pixelDistance * 2.0);
+                lumi = max(0.0, lumi);
+                
+                float3 newColor = gradientColor * lumi;
+                float4 final = float4(newColor.x, newColor.y, newColor.z, lumi);
+
+                // https://alastaira.wordpress.com/2015/08/07/unity-shadertoys-a-k-a-converting-glsl-shaders-to-cghlsl/
+                float4 finalColor = lerp(col, final, lumi) * col.w;
+
+                // make alpha be a function of distance from center
+                finalColor.a = pixelDistance / 4.0;
+
+                return finalColor;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/Scenes/scripts/RipplesVisualization.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RipplesVisualization : VisualizerMultiple
+{
+    public bool placeInFrontOfCamera;
+    //Camera camera; // TODO: remove? we always assume there's only one camera and it's the main cam
+    public float distFromCamera;
+    public Material shaderMaterial;
+    public Color color; // new Vector4(0.3f, 0.6f, 1, 0.8f)
+
+    private float[] spectrumData;
+    private float[] prevSpectrumData;   // keep track of previous spectrum data
+    private GameObject parent;
+
+    private float timeToWait = 0.3f; // wait n sec before updating visualization
+    private float timeElapsed = 0f;
+    private bool started = false;
+    private void setupSpectrumDataPoints()
+    {
+        int sampleRate = AudioSettings.outputSampleRate;
+        int freqMax = sampleRate / 2; // this is the max supported frequency in our data
+        float freqPerIndex = freqMax / sampleDataSize; // this is the frequency increment between indices of the data. e.g. data[0] = n Hz, data[1] = 2*n Hz, etc.
+
+        int targetIndexMin = (int)(desiredFreqMin / freqPerIndex);
+        int targetIndexMax = (int)Math.Min(sampleDataSize - 1, desiredFreqMax / freqPerIndex);
+        int numFreqBands = targetIndexMax - targetIndexMin;
+
+        // assign random locations to each sphere representing a freq bin
+        //float xInterval = 1f/numFreqBands + 0.01f;
+        //float xStart = 0f;
+        for(int i = 0; i < numFreqBands; i++)
+        {
+            float yPos = UnityEngine.Random.Range(0f, 1f);
+            float xPos = UnityEngine.Random.Range(0f, 1f);
+            float zPos = 60f;
+
+            //xStart += xInterval;
+
+            Vector3 newPos = Camera.main.ViewportToWorldPoint(new Vector3(xPos, yPos, zPos));
+
+            GameObject newRipple = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            newRipple.transform.position = newPos;
+            newRipple.transform.Rotate(new Vector3(-90, 0, 0));
+            //newRipple.transform.rotation = UnityEngine.Random.rotation;
+            newRipple.transform.parent = parent.transform;
+
+            newRipple.GetComponent<Renderer>().material = shaderMaterial;
+            newRipple.GetComponent<Renderer>().material.SetFloat("_Speed", 1.1f);
+            newRipple.GetComponent<Renderer>().material.SetFloat("_Density", 60f);
+            newRipple.GetComponent<Renderer>().material.SetFloat("_Strength", 2f);
+            newRipple.GetComponent<Renderer>().material.SetFloat("_Brightness", 1f);
+            newRipple.GetComponent<Renderer>().material.SetVector("color", color);
+            newRipple.GetComponent<Renderer>().material.SetVector("center", new Vector2(0.5f, 0.5f));
+
+            objectsArray.Add(newRipple);
+            isAnimatingArray.Add(false);
+        }
+    }
+
+    public void displaySpectrum(float[] spectrumData)
+    {
+        int sampleRate = AudioSettings.outputSampleRate;
+        int freqMax = sampleRate / 2;                       // this is the max supported frequency in our data
+        float freqPerIndex = freqMax / sampleDataSize;      // this is the frequency increment between indices of the data. e.g. data[0] = n Hz, data[1] = 2*n Hz, etc.
+
+        int targetIndexMin = (int)(desiredFreqMin / freqPerIndex);
+        int targetIndexMax = (int)Math.Min(sampleDataSize - 1, desiredFreqMax / freqPerIndex);
+
+        int particleIndex = 0;
+        for (int i = targetIndexMin; i < targetIndexMax; i++)
+        {
+            GameObject currObj = objectsArray[particleIndex];
+            Transform currTransform = currObj.transform;
+            float binValDelta = 90 * (spectrumData[i] - prevSpectrumData[i]);
+
+            // TODO: using binValDelta, pass to currObj's shader
+            // https://github.com/twostraws/ShaderKit/blob/main/Shaders/SHKCircleWaveBlended.fsh
+            // https://answers.unity.com/questions/1409060/is-there-any-way-to-create-a-dynamic-material-when.html
+            // https://answers.unity.com/questions/55402/how-to-pass-custom-variables-to-shaders.html
+
+            if(Mathf.Abs(binValDelta) < 1f)
+            {
+                currObj.GetComponent<Renderer>().material.SetVector("color", new Vector4(1f, 1f, 1f, 0f));
+            }
+            else
+            {
+                currObj.GetComponent<Renderer>().material.SetVector("color", color);
+            }
+
+            currTransform.GetComponent<Renderer>().material.SetFloat("freqBinDelta", binValDelta);
+
+            particleIndex++;
+        }
+
+        spectrumData.CopyTo(prevSpectrumData, 0);
+    }
+    public override void Start()
+    {
+        base.Start();
+        parent = new GameObject();
+        spectrumData = audioData;
+        prevSpectrumData = new float[sampleDataSize];
+        prefill(prevSpectrumData, 0.0f);
+
+        setupSpectrumDataPoints();
+    }
+
+    void Update()
+    {
+        if (timeElapsed >= timeToWait || !started)
+        {
+            audioSrc.GetSpectrumData(spectrumData, 0, FFTWindow.BlackmanHarris);
+            displaySpectrum(spectrumData);
+            timeElapsed = 0f;
+            started = true;
+        }
+
+        timeElapsed += Time.deltaTime;
+    }
+}
